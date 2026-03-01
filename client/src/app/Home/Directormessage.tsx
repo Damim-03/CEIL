84ec6c559da1689cc2c58f2246fbf66d7ccba763
Damim-03 <imadeddinekir95@gmail.com>
2026-03-01 01:19:56 +0100

    add pictures

--- client/src/app/Home/Directormessage.tsx
+++ client/src/app/Home/Directormessage.tsx
@@ -2,7 +2,7 @@ import { Quote, GraduationCap } from "lucide-react";
 import { useLanguage } from "../../hooks/useLanguage";
 import { useState, useEffect, useRef } from "react";
 
-import directorPhoto from "../../assets/logo.jpg";
+import directorPhoto from "../../assets/Derictore.jpg";
 
 export function DirectorMessage() {
   const { t, dir, isRTL } = useLanguage();
